5fe133195e28f994144a614b9b9e17cb73b66099
João Victor <jujotasurf@gmail.com>
2022-01-06 22:52:47 -0300

    Exercicios

---
+++ Xenlongao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+namespace Dio
+{
+       class Xenlongao
+    {
+        static void Main(string[] args)
+        {
+          var testes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testes; i++)
+            {
+              var esferas = int.Parse(Console.ReadLine());
+              Console.WriteLine(esferas - Math.Floor(Math.Sqrt(esferas)));
+            }
+        }
+    }
+}
